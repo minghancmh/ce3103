796b5a14e9376d7e6d61e5e3ae99a4fc9f26f28b
Chan Ming Han <chanminghan00@gmail.com>
2024-10-11 12:28:05 +0800

    feat: implement multithreading on server

--- Lab4/src/raspiServer/server.c
+++ Lab4/src/raspiServer/server.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 200
+#define BUFFER_SIZE 1024
 #define PORT 8080
 
 int main() {
@@ -14,15 +14,21 @@ int main() {
 
   char buffer[BUFFER_SIZE];
 
+  // Creates a socket with TCP, IPv4 configuration
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("Socket creation failed.");
     exit(EXIT_FAILURE);
   }
 
   address.sin_family = AF_INET;
+
+  // bind to all available network interfaces on the machine
   address.sin_addr.s_addr = INADDR_ANY;
+
+  // host endian to network endian conversion
   address.sin_port = htons(PORT);
 
+  // Binds the address specified in &address to the socket created previously
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
     perror("Bind failed");
     close(server_fd);
@@ -30,41 +36,63 @@ int main() {
   }
 
   // listen for incoming connections
+  // max len of queue for incoming connections (3 connections max, anything
+  // beyond that, we throw ERRCONNREFUSED)
   if (listen(server_fd, 3) < 0) {
     perror("Listen failed");
     close(server_fd);
     exit(EXIT_FAILURE);
   }
 
-  printf("Server listening on port %d\n", PORT);
-
-  // Accept new incoming connetions
-  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                           (socklen_t *)&addr_len)) < 0) {
-    perror("accepting new incoming connection failed");
-    close(server_fd);
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Connection established with client!\n");
-  char *server_ack_msg = "Server Acknowledged!";
-
   while (1) {
-    int bytes_read = recv(new_socket, buffer, BUFFER_SIZE, 0);
-    printf("bytes read: %d\n", bytes_read);
-    if (bytes_read <= 0) {
-      perror("read failed, or connection closed");
-      break;
-    }
-    buffer[bytes_read] = '\0';
-    printf("Message received from client: %s\n", buffer);
+    pid_t pp = getpid();
+    printf("[%d]: Server listening on port %d\n", pp, PORT);
 
-    int bytes_sent =
-        send(new_socket, server_ack_msg, strlen(server_ack_msg), 0);
-    if (bytes_sent > 0) {
-      printf("server succesfully sent ack\n");
+    // Accept new incoming connetions
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addr_len)) < 0) {
+      perror("accepting new incoming connection failed");
+      continue;
+    }
+
+    // Fork so that we can handle multiple connections
+    pid_t pid = fork();
+    if (pid < 0) {
+      perror("Forking failed");
+    } else if (pid == 0) {
+      // we are in the child
+      close(server_fd);
+      pid_t p = getpid();
+      printf("[%d]: Connection established with client!\n", p);
+      char *server_ack_msg = "Server Acknowledged!";
+
+      while (1) {
+        int bytes_read = recv(new_socket, buffer, BUFFER_SIZE, 0);
+
+        printf("[%d]: bytes read: %d\n", p, bytes_read);
+        if (bytes_read <= 0) {
+          perror("read failed, or connection closed");
+          break;
+        }
+        buffer[bytes_read] = '\0';
+        printf("[%d]: Message received from client: %s\n", p, buffer);
+
+        int bytes_sent =
+            send(new_socket, server_ack_msg, strlen(server_ack_msg), 0);
+        if (bytes_sent > 0) {
+          printf("[%d]: server succesfully sent ack\n", p);
+        }
+      }
+      close(new_socket);
+      exit(0); // exit child process when done
+    } else {
+      // we are in the parent
+      close(new_socket);
+      // TODO: should implement wait for the parent to wait for child, lest they
+      // become zombie processes
     }
   }
+  close(server_fd);
 
   return 0;
 }
--- Lab4/src/ubuntuClient/client.c
+++ Lab4/src/ubuntuClient/client.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFSIZE 200
+#define BUFSIZE 1024
 #define PORT 8080
 #define SERVER_IP "172.17.0.3"
 
@@ -10,9 +10,19 @@ int main() {
 
   int sock = 0;
   struct sockaddr_in server_address;
+
+  // Receiver buffer
   char buf[BUFSIZE];
+
+  // Sender buffer
   char message[BUFSIZE];
 
+  /*
+  Creating a new socket with the following configuration
+    - AF_INET: IPv4 Address Family
+    - SOCK_STREAM: Sequenced, 2 way byte streams
+    - 0: Default value (AF_INET + SOCK_STREAM => TCP)
+  */
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("Socket creation failed");
     return -1;
@@ -21,12 +31,13 @@ int main() {
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(PORT);
 
+  // Converting string representation of IP to network format, stores it in server_address.sin_addr
   if (inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr) <= 0) {
     perror("Invalid address / address not supported");
     return -1;
   }
 
-  // Connecting to the server
+  // Connecting to the server on the socked created above
   if (connect(sock, (struct sockaddr *)&server_address,
               sizeof(server_address)) < 0) {
     perror("connection failed");
@@ -35,7 +46,6 @@ int main() {
 
   printf("Connected to server. Type a message and press Enter: \n");
 
-  // int i;
 
   while (1) {
     fgets(message, BUFSIZE, stdin);
